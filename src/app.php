7bdd3a04fe6171e93210bd8122f7685d65e20dfd
Sergey Polischuk <spolischook@gmail.com>
2013-12-29 00:25:56 +0200

    Added app.php and include it into front controller

---
+++ src/app.php
@@ -0,0 +1,7 @@
+<?php
+
+$app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
--- web/index.php
+++ web/index.php
@@ -6,4 +6,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+require __DIR__ . '/../app/config/dev.php';
+require __DIR__ . '/../src/app.php';
 $app->run();
