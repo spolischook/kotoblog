3afdb61a60eff211af3546a32dc58c72784db3f0
Sergey Polischuk <spolischook@gmail.com>
2013-12-30 02:28:45 +0200

    Added Twig provider to application

--- src/app.php
+++ src/app.php
@@ -5,3 +5,11 @@ $app->register(new Silex\Provider\FormServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.options' => array(
+        'cache' => isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
+        'strict_variables' => true,
+    ),
+    'twig.path' => array(__DIR__ . '/../app/views')
+));
