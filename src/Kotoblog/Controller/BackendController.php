40cacd76a53b8b7d59c28daff8bdff05a16217a2
Sergey Polischuk <spolischook@gmail.com>
2014-01-06 03:58:10 +0200

    Added Backend for Article with pagination

---
+++ src/Kotoblog/Controller/BackendController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Kotoblog\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
+use Kotoblog\Form\ArticleType;
+
+class BackendController
+{
+    protected $perPageDefault = 10;
+
+    public function indexAction(Request $request, Application $app)
+    {
+        return $app['twig']->render('Backend/index.html.twig');
+    }
+
+    public function articlesAction(Request $request, Application $app)
+    {
+        $perPage = $request->query->get('count') ? $request->query->get('count') : $this->perPageDefault;
+        $page    = $request->query->get('page') ? $request->query->get('page') : 1;
+
+        $articles   = $app['repository.blogpost']->findAll($perPage, ($page-1)*$perPage, array('created_at' => 'DESC'));
+        $entryCount = $app['repository.blogpost']->getCount();
+
+        return $app['twig']->render('Backend/Article/articles.html.twig', array(
+            'articles'   => $articles,
+            'entryCount' => $entryCount,
+        ));
+    }
+
+    public function editArticleAction(Request $request, Application $app, $slug)
+    {
+        $article = $app['repository.blogpost']->find($slug);
+
+        if (!$article) {
+            $app->abort(404, sprintf('The requested article with slug "%s" was not found.', $slug));
+        }
+
+        $form = $app['form.factory']->create(new ArticleType(), $article);
+
+        $data = array(
+            'form' => $form->createView(),
+            'title' => 'Edit article ' . $article->getTitle(),
+        );
+
+        return $app['twig']->render('Backend/Article/form.html.twig', $data);
+    }
+
+    public function paginationAction(Request $request, Application $app)
+    {
+        $entryCount = $request->query->get('entry-count');
+
+        if (!$entryCount) {
+            throw new MissingMandatoryParametersException('You must specify "entry-count" parameter to render pagination');
+        }
+
+        $perPage = $request->query->get('count') ? $request->query->get('count') : $this->perPageDefault;
+        $page    = $request->query->get('page') ? $request->query->get('page') : 1;
+        $requestUrl = $request->query->get('requestUrl');
+
+        return $app['twig']->render('Backend/pagination.html.twig', array(
+            'entryCount' => $entryCount,
+            'perPage'    => $perPage,
+            'page'       => $page,
+            'requestUrl' => $requestUrl,
+        ));
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -9,3 +9,13 @@ $app->match('/login', 'Kotoblog\Controller\UserController::loginAction')
     ->bind('login');
 $app->get('/logout', 'Kotoblog\Controller\UserController::logoutAction')
     ->bind('logout');
+
+$app->get('/admin', 'Kotoblog\Controller\BackendController::indexAction')
+    ->bind('adminIndex');
+$app->get('/admin/articles', 'Kotoblog\Controller\BackendController::articlesAction')
+    ->bind('adminArticles');
+$app->get('/admin/articles/{slug}', 'Kotoblog\Controller\BackendController::editArticleAction')
+    ->bind('adminArticle');
+
+$app->get('/admin/pagination', 'Kotoblog\Controller\BackendController::paginationAction')
+    ->bind('adminPagination');
