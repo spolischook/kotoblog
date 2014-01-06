020119bdd1db2554de97fdec91b3d16993a1eb74
Sergey Polischuk <spolischook@gmail.com>
2014-01-06 03:52:28 +0200

    Updated login page.

--- src/Kotoblog/Controller/UserController.php
+++ src/Kotoblog/Controller/UserController.php
@@ -4,21 +4,28 @@ namespace Kotoblog\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 class UserController
 {
     public function loginAction(Request $request, Application $app)
     {
+        if ($app['security']->getToken()) {
+            return $app->redirect($app['url_generator']->generate('homepage'));
+        }
+
         $form = $app['form.factory']->createBuilder('form')
-            ->add('username', 'text', array('label' => 'Username', 'data' => $app['session']->get('_security.last_username')))
-            ->add('password', 'password', array('label' => 'Password'))
-            ->add('login', 'submit')
+            ->add('username', 'text', array('label' => 'Username', 'attr' => array('class' => 'form-control')))
+            ->add('password', 'password', array('label' => 'Password', 'attr' => array('class' => 'form-control')))
+            ->add('login', 'submit', array('attr' => array('class' => 'btn btn-lg btn-primary btn-block')))
             ->getForm();
 
         $data = array(
-            'form'  => $form->createView(),
+            'last_username' => $app['session']->get('_security.last_username'),
             'error' => $app['security.last_error']($request),
+            'form' => $form->createView(),
         );
+
         return $app['twig']->render('login.html.twig', $data);
     }
 
--- src/app.php
+++ src/app.php
@@ -5,11 +5,38 @@ $app->register(new Silex\Provider\FormServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider());
+
+$app['loginCheckRoute'] = '/admin/login_check';
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'main' => array(
+            'pattern' => '^/admin',
+            'form' => array(
+                'login_path' => '/login',
+                'check_path' => '/admin/login_check',
+                'username_parameter' => 'form[username]',
+                'password_parameter' => 'form[password]',
+            ),
+            'logout' => array('logout_path' => '/admin/logout'),
+            'users' => array(
+                'admin' => array('ROLE_ADMIN', 'Nr4mS9Hl0a41Wb6Iu8ONZf2C97Txq7U8BUyJMIrhHfHXZU05+6qgrzzhuAvcuagrIqXhMdulsSj3Ba5GYTd91Q=='),
+            ),
+        ),
+    ),
+    'security.role_hierarchy' => array(
+        'ROLE_ADMIN' => array('ROLE_USER'),
+    ),
+));
+$app['security.access_rules'] = array(
+    array('^/admin', 'ROLE_ADMIN'),
+);
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.options' => array(
         'cache' => isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
         'strict_variables' => true,
+        'globals' => array('loginCheckRoute' => 'wXMgt4EB3pG9Jki35t6bpGLJMrkQces6ETTE9fkQ4JM'),
     ),
     'twig.path' => array(__DIR__ . '/../app/views')
 ));
