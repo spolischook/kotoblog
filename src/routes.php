8b8adae4040a9d2b84ce6baa46f0fee9f2cc1b66
Sergey Polischuk <spolischook@gmail.com>
2014-01-11 06:25:49 +0200

    Added enabled coocies action

--- src/routes.php
+++ src/routes.php
@@ -21,8 +21,14 @@ $app->get('/admin', 'Kotoblog\Controller\BackendController::indexAction')
     ->bind('adminIndex');
 $app->get('/admin/articles', 'Kotoblog\Controller\BackendController::articlesAction')
     ->bind('adminArticles');
-$app->get('/admin/articles/{slug}', 'Kotoblog\Controller\BackendController::editArticleAction')
-    ->bind('adminArticle');
+$app->match('/admin/articles/{slug}', 'Kotoblog\Controller\BackendController::editArticleAction')
+    ->bind('adminArticle')
+    ->method('GET|POST');
 
 $app->get('/admin/pagination', 'Kotoblog\Controller\BackendController::paginationAction')
     ->bind('adminPagination');
+
+$app->get('/enable-cookies', function () use ($app) {
+    return $app['twig']->render('enable-cookies.html.twig');
+})
+    ->bind('enableCookies');
