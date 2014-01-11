d05af900c9d0c15a0d7658535dfe95e5ec074ee1
Sergey Polischuk <spolischook@gmail.com>
2014-01-11 06:31:49 +0200

    Updated backend

--- src/Kotoblog/Controller/BackendController.php
+++ src/Kotoblog/Controller/BackendController.php
@@ -21,7 +21,7 @@ class BackendController
         $perPage = $request->query->get('count') ? $request->query->get('count') : $this->perPageDefault;
         $page    = $request->query->get('page') ? $request->query->get('page') : 1;
 
-        $articles   = $app['repository.article']->findAll($perPage, ($page-1)*$perPage, array('created_at' => 'DESC'));
+        $articles   = $app['repository.article']->findBy(array(), array('created_at' => 'DESC'), $perPage, ($page-1)*$perPage);
         $entryCount = $app['repository.article']->getCount();
 
         return $app['twig']->render('Backend/Article/articles.html.twig', array(
@@ -32,18 +32,31 @@ class BackendController
 
     public function editArticleAction(Request $request, Application $app, $slug)
     {
-        $article = $app['repository.article']->find($slug);
+        $article = $app['repository.article']->findOneBy(array('slug' => $slug));
 
         if (!$article) {
             $app->abort(404, sprintf('The requested article with slug "%s" was not found.', $slug));
         }
 
-        $form = $app['form.factory']->create(new ArticleType(), $article);
+        $form = $app['form.factory']->create($app['form_type.article'], $article);
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $article = $app['repository.article']->save($form->getData());
+                $message = 'The article "' . $article->getTitle() . '" has been saved.';
+                $app['session']->getFlashBag()->add('success', $message);
+
+                return $app->redirect($app['url_generator']->generate('adminArticle', array('slug' => $article->getSlug())));
+            }
+        }
 
         $data = array(
             'form' => $form->createView(),
             'title' => 'Edit article ' . $article->getTitle(),
         );
+
 
         return $app['twig']->render('Backend/Article/form.html.twig', $data);
     }
