34c98c4f9c8da756b7eb4702524644c927442aea
Sergey Polischuk <spolischook@gmail.com>
2014-01-11 05:18:54 +0200

    Splited to two enviroment dev and prod

--- web/index.php
+++ web/index.php
@@ -6,8 +6,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-require __DIR__ . '/../app/config/dev.php';
+require __DIR__ . '/../app/config/prod.php';
 require __DIR__ . '/../src/app.php';
 require __DIR__ . '/../src/routes.php';
+
+$app->error(function (\Exception $e, $code) use ($app) {
+    switch ($code) {
+        case 404:
+            $message = '404 - The requested page could not be found.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong.';
+    }
+
+    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
 
 $app->run();
