d9f107adbbc9d823f0c3a77c2e8a4f7af8e1f5f3
Sergey Polischuk <spolischook@gmail.com>
2014-01-11 06:16:43 +0200

    Added TagTransformer

---
+++ src/Kotoblog/Form/TagTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kotoblog\Form;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Kotoblog\Entity\Tag;
+use Kotoblog\Repository\TagRepository;
+
+class TagTransformer implements DataTransformerInterface
+{
+    /** @var TagRepository */
+    private $tagRepository;
+
+    public function __construct($tagRepository)
+    {
+        $this->tagRepository = $tagRepository;
+    }
+
+    /**
+     * @param ArrayCollection $tags
+     * @return mixed|string
+     */
+    public function transform($tags)
+    {
+        if (!$tags || $tags->isEmpty()) {
+            return "";
+        }
+
+        return implode(',', $tags->toArray());
+    }
+
+    public function reverseTransform($tagsString)
+    {
+        if (!$tagsString) {
+            return;
+        }
+
+        $tagsArray = explode (',', $tagsString);
+        $tagObjects = array();
+
+        foreach ($tagsArray as $tagTitle) {
+            $tagObjects[] = $this->tagRepository->findOneByTitleOrCreateNewIfNotExist(trim($tagTitle));
+        }
+
+        return new ArrayCollection($tagObjects);
+    }
+}
